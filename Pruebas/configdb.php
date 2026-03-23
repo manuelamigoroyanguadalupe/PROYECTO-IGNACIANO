d980614b99a69d58a5b05f3d1df054e12cd19cf6
manuelamigoroyanguadalupe <manuelamigoroyan.guadalupe@alumnado.fundacionloyola.net>
2026-03-23 13:01:55 +0100

    Update database configuration for production

---
+++ Pruebas/configdb.php
@@ -0,0 +1,8 @@
+<?php
+    define("SERVIDOR", '1daw.esvirgua.com');
+    define("USUARIO", '01@1daw.esvirgua.com');
+    define("PASSWORD", '.5axu-kb-@~NEck"');
+    define("BBDD", 'ser1daw_BD1-01');
+    ?>
+    
+<!-- configdb.php     -->
